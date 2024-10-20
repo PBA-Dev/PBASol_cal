df14982b19578e44a8c8a497128cfd786607ba99
Alex <alex.torrescanety@optimum-pflegeberatung.de>
2024-10-20 17:48:00 +0000

    fix login

--- routes.py
+++ routes.py
@@ -74,8 +74,8 @@ def init_routes(app):
             else:
                 logging.debug('Ungültiger Anmeldeversuch')
                 flash('Ungültiger Benutzername oder Passwort', 'danger')
-        csrf_token = generate_csrf()
-        return render_template('login.html', csrf_token=csrf_token)
+        
+        return render_template('login.html')
 
     @app.route('/logout')
     @login_required
